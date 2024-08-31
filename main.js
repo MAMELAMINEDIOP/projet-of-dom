2843deef05a760d43629aa498ad1c75ef9650a97
MAMELAMINEDIOP <mamelaminediop9@gmail.com>
2024-08-31 22:59:46 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+//var selectorByiiId = document. getelementById("pro")
+//var selectorBytype = document. getElementsByTagName("li")
+
+var selectorByclasse = document.getElementsByClassName("main")
+var atributelement = selectorByclasse[0].getAttribute("class")
+var children = selectorByclasse[0].children
+console.log(children);
